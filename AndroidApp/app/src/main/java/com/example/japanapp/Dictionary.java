a4417fe7504bfaee744a0613af515c9b6f266364
Sascha Wombacher <sascha.wombacher@gmx.de>
2020-03-01 22:23:47 +0100

    add simple dictionary implementation

--- AndroidApp/app/src/main/java/com/example/japanapp/Dictionary.java
+++ AndroidApp/app/src/main/java/com/example/japanapp/Dictionary.java
@@ -1,13 +1,35 @@
 package com.example.japanapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class Dictionary extends AppCompatActivity {
+
+    void addButtonHandlers() {
+        findViewById(R.id.m_BtnDictionarySearch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText)findViewById(R.id.m_DictionarySearchField);
+                String searchText = editText.getText().toString();
+                String searchResult = "";
+                if (((RadioButton)findViewById(R.id.m_DictionaryRadioButtonEnglish)).isChecked())
+                    searchResult = m_Translator.translateEnglish(searchText);
+                else if (((RadioButton)findViewById(R.id.m_DictionaryRadioButtonKana)).isChecked())
+                    searchResult = m_Translator.translateKana(searchText);
+                else
+                    Log.e("Dictionary", "unknown search language");
+
+                ((TextView)findViewById(R.id.m_DictionaryTextViewResult)).setText(searchResult);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +38,9 @@ public class Dictionary extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.m_Translator = LogicHandlerAbstraction.getTranslator();
+        addButtonHandlers();
     }
 
+    com.cpp.shared.VocabularyTranslator m_Translator;
 }
